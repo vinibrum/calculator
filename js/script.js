aa79f73fada4c604822d81210bcb4e9fcec96166
Vinícius Brum <brumc.vini@gmail.com>
2020-10-06 20:55:43 -0300

    Adicionando separador de milhar a medida que vai crescendo no display da calculadora.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 var elements = document.querySelectorAll('.num-bttn');
+
 elements.forEach(function(el){
     el.addEventListener('click', function(){
         let display = document.getElementById('display');
-        if(display.innerText.length < 15)
-            //if(display.innerText.length > 0 && display.innerText.length % 3 == 0)  
-            display.innerText += el.innerText;
+        if(display.innerText.length == 0 && el.innerText == '0')    return;
+        if(display.innerText.length < 15){
+            let strAux = display.innerText.replaceAll('.','');
+            console.log('straux');
+            console.log(strAux);
+            let newDisplayInfo = strAux + el.innerText;
+            let resp = '';
+            let cont = 0;
+            let aux = '';
+            
+            for(let i = newDisplayInfo.length - 1; i >= 0; i--){
+                if(cont == 3){
+                    aux = '.';
+                    aux += resp;
+                    resp = aux;
+                    cont = 0;
+                }
+                aux = newDisplayInfo.charAt(i);
+                aux += resp;
+                resp = aux;
+                cont++;
+            }
+            display.innerText = resp;
+        }
     });
 });
 
@@ -13,10 +35,3 @@ el.addEventListener('click', function(){
     let display = document.getElementById('display');
     display.innerText = display.innerText.substring(0, display.innerText.length - 1);
 });
-
-/*
-el.addEventListener('click', function(){
-    let display = document.getElementById('display');
-    display.innerText += el.innerText;
-});
-*/
