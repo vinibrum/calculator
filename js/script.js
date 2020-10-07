bbbec172b9cd9760faba8d0a6148f8d9bf90a669
Vinícius Brum <brumc.vini@gmail.com>
2020-10-07 19:58:41 -0300

    Adicionando zero como valor inicial do display e adicionando separador de milhar a medidade que um número é excluído do display.

--- js/script.js
+++ js/script.js
@@ -4,28 +4,11 @@ elements.forEach(function(el){
     el.addEventListener('click', function(){
         let display = document.getElementById('display');
         if(display.innerText.length == 0 && el.innerText == '0')    return;
+        if(display.innerText == '0')    display.innerText = '';
         if(display.innerText.length < 15){
             let strAux = display.innerText.replaceAll('.','');
-            console.log('straux');
-            console.log(strAux);
             let newDisplayInfo = strAux + el.innerText;
-            let resp = '';
-            let cont = 0;
-            let aux = '';
-            
-            for(let i = newDisplayInfo.length - 1; i >= 0; i--){
-                if(cont == 3){
-                    aux = '.';
-                    aux += resp;
-                    resp = aux;
-                    cont = 0;
-                }
-                aux = newDisplayInfo.charAt(i);
-                aux += resp;
-                resp = aux;
-                cont++;
-            }
-            display.innerText = resp;
+            display.innerText = addSeparator(newDisplayInfo);
         }
     });
 });
@@ -33,5 +16,30 @@ elements.forEach(function(el){
 var el = document.getElementById('backspace-bttn');
 el.addEventListener('click', function(){
     let display = document.getElementById('display');
-    display.innerText = display.innerText.substring(0, display.innerText.length - 1);
+    let strAux = display.innerText.replaceAll('.', '');
+    let newDisplayInfo = strAux.substring(0, strAux.length - 1);
+    let resp = addSeparator(newDisplayInfo);
+    
+    if(resp.length == 0)    display.innerText = '0';
+    else                    display.innerText = resp;
+    
 });
+
+function addSeparator(str){
+    let resp = '';
+    let cont = 0;
+    let aux = '';
+    for(let i = str.length - 1; i >= 0; i--){
+        if(cont == 3){
+            aux = '.';
+            aux += resp;
+            resp = aux;
+            cont = 0;
+        }
+        aux = str.charAt(i);
+        aux += resp;
+        resp = aux;
+        cont++;
+    }
+    return resp;
+}
